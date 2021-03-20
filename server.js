e8f7f27a787be65f06d0fd842c7e8bd2ed36adf5
admin <admin@elizabeth.local>
2021-03-20 16:39:09 -0500

    local/heroku

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 
-const PORT = process.env.PORT || 3000; 
+const PORT = process.env.PORT || 7000; 
 
 const app = express();
 
@@ -14,14 +14,15 @@ app.use(express.json());
 app.use(express.static("public"));
 
 mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://localhost/workout',
+  process.env.MONGODB_URI || 'mongodb+srv://liz:Surgery.7@cluster0.cystd.mongodb.net/liz?retryWrites=true&w=majority',
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
     useFindAndModify: false
   }
-);
+).then(()=>console.log('DB CONNECTION ESTABLISHED'))
+.catch(err=>console.log("DB CONNETION FAILED!", err))
 
 // routes
 app.use(require("./routes/api.js"));
